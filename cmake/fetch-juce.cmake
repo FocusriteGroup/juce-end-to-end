c65a8a559e36340fad97dffa7c43d278e45985d2
Joe Noël <joe.noel@focusrite.com>
2021-10-20 10:58:59 +0100

    Technical/circle ci (#5)
    * Create Circle CI config
    
    * Get prettier config from public GitHub URL
    
    * Install CMake
    
    * Add step to build library
    
    * Use Xcode generator
    
    * Add Windows build
    
    * Fetch CMake directly
    
    * Don't clone JUCE's submodules
    
    * Log tar version
    
    * Use tar library to create archive

--- cmake/fetch-juce.cmake
+++ cmake/fetch-juce.cmake
@@ -1,6 +1,6 @@
 include (FetchContent)
 fetchcontent_declare (juce GIT_REPOSITORY https://github.com/juce-framework/JUCE
-                      GIT_TAG 6.1.2)
+                      GIT_TAG 6.1.2 GIT_SUBMODULES "")
 
 fetchcontent_getproperties (juce)
 fetchcontent_makeavailable (juce)
