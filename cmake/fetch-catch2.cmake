c349034ca7ac4a1533432bc9c74e9351df0dc41e
Joe Noël <joe.noel@focusrite.com>
2021-10-22 18:42:17 +0100

    Don't fetch Catch2 submodules

--- cmake/fetch-catch2.cmake
+++ cmake/fetch-catch2.cmake
@@ -2,6 +2,6 @@ include (FetchContent)
 
 fetchcontent_declare (
   Catch2 GIT_REPOSITORY https://github.com/catchorg/Catch2.git
-  GIT_TAG v3.0.0-preview3)
+  GIT_TAG v3.0.0-preview3 GIT_SUBMODULES "")
 
 fetchcontent_makeavailable (Catch2)
