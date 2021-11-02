3ac3367714796e7d5da6f94d288cb2a5add4c016
Joe Noël <joe.noel@focusrite.com>
2021-11-02 11:11:49 +0000

    Remove version from exported CMake config

--- cmake/AmpifyE2EConfig.cmake
+++ cmake/AmpifyE2EConfig.cmake
@@ -2,7 +2,6 @@ find_package (PkgConfig)
 pkg_check_modules (PC_AMPIFY_E2E QUIET AmpifyE2E)
 
 set (AMPIFY_E2E_ROOT_DIR ${AmpifyE2E_DIR}/../..)
-set (AMPIFY_E2E_VERSION 0.0.0)
 
 if (APPLE)
   set (LIBRARY_LOCATION ${AMPIFY_E2E_ROOT_DIR}/lib/libampify-e2e.a)
@@ -12,11 +11,10 @@ elseif (WIN32)
   set (LIBRARY_LOCATION_DEBUG ${AMPIFY_E2E_ROOT_DIR}/lib/ampify-e2ed.lib)
 endif ()
 
-mark_as_advanced (AMPIFY_E2E_FOUND AMPIFY_E2E_ROOT_DIR AMPIFY_E2E_VERSION)
+mark_as_advanced (AMPIFY_E2E_FOUND AMPIFY_E2E_ROOT_DIR)
 
 include (FindPackageHandleStandardArgs)
-find_package_handle_standard_args (AmpifyE2E REQUIRED_VARS AMPIFY_E2E_ROOT_DIR
-                                   VERSION_VAR AMPIFY_E2E_VERSION)
+find_package_handle_standard_args (AmpifyE2E REQUIRED_VARS AMPIFY_E2E_ROOT_DIR)
 
 if (AmpifyE2E_FOUND AND NOT ampify-e2e::ampify-e2e)
   add_library (ampify-e2e::ampify-e2e STATIC IMPORTED)
