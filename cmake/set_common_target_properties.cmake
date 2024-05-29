3a98738c8382b559f523b27b6871034112e7d166
James J <james.johnson@focusrite.com>
2024-05-29 15:45:00 +0100

    Check for Clang compilers, not macOS to set compiler flags

--- cmake/set_common_target_properties.cmake
+++ cmake/set_common_target_properties.cmake
@@ -4,10 +4,10 @@ function (set_common_target_properties TARGET)
 
   target_compile_definitions (${TARGET} PRIVATE $<$<CONFIG:Debug>:DEBUG>)
 
-  if (APPLE)
+  if ((${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang") OR (${CMAKE_CXX_COMPILER_ID}
+                                                      STREQUAL "AppleClang"))
     target_compile_options (${TARGET} PRIVATE -Wall -Wextra -Werror)
   endif ()
-
   if (MSVC)
     target_compile_options (${TARGET} PRIVATE /W4 /WX)
   endif ()
