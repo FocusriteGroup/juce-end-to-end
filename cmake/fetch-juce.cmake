6b4b3c85ce34d7911dd99b001aef4bc824636ea9
James J <james.johnson@focusrite.com>
2025-04-30 16:29:54 +0100

    Migrate from Circle to GHA

--- cmake/fetch-juce.cmake
+++ cmake/fetch-juce.cmake
@@ -1,5 +1,8 @@
 include (FetchContent)
-fetchcontent_declare (juce GIT_REPOSITORY https://github.com/juce-framework/JUCE
-                      GIT_TAG 7.0.9 GIT_SUBMODULES "")
 
+fetchcontent_declare (juce
+  GIT_REPOSITORY https://github.com/juce-framework/JUCE
+  GIT_TAG 8.0.7
+  GIT_SUBMODULES ""
+)
 fetchcontent_makeavailable (juce)
